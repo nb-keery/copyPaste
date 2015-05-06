26311429dcf6cfe11751e863b428b04e0909cca4
navec <navecbatchi@gmail.com>
2015-05-06 22:15:07 +0200

    maj
    maj

--- Memo/conf/config.php
+++
@@ -1,8 +0,0 @@
-<?php $dbh = new PDO('mysql:host=localhost;dbname=memo', 'root', '', array(
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO:: FETCH_ASSOC,
-		PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8" ));
-
-		define("USER", "copyPaste");
-
-		session_start();
--- read_me.txt
+++
@@ -1,6 +0,0 @@
-echo # copyPaste >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/nb-keery/copyPaste.git
-git push -u origin master
